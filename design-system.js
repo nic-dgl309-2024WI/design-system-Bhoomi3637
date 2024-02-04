8d159b60f35a04b6b956dd073fbbf9083145e034
Bhoomi3637 <bhoomipatel381@gmail.com>
2024-02-03 17:12:29 -0800

    Add javascript for toggel for mobile  view

---
+++ design-system.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+    var content = document.getElementById("content");
+    content.style.display === "block" ? content.style.display = "none" : content.style.display = "block";
+  }
