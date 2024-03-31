13c7d094b2e3aab351b320e6a5cf31f061b889ee
Bhoomi3637 <bhoomipatel381@gmail.com>
2024-03-30 23:26:20 -0700

    Add a JavaScript for accordion

--- design-system.js
+++ design-system.js
@@ -28,3 +28,17 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
+var acc = document.getElementsByClassName("product__category--accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
